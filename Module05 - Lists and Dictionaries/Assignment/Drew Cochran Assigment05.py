6860ef72481e46da643da479f23bb9f9439872a2
drewber78 <drew.j.cochran@gmail.com>
2023-02-10 14:32:08 -0600

    Initial commit for work on Assignment05 starter.py program. Not tested. Will need to test upon return from vacation.

---
+++ Module05 - Lists and Dictionaries/Assignment/Drew Cochran Assigment05.py
@@ -0,0 +1,152 @@
+# ------------------------------------------------------------------------ #
+# Title: Assignment 05
+# Description: Working with Dictionaries and Files
+#              When the program starts, load each "row" of data
+#              in "ToDoToDoList.txt" into a python Dictionary.
+#              Add the each dictionary "row" to a python list "table"
+# ChangeLog (Who,When,What):
+# Drew Cochran, 10FEB2023, Created starter code from Randall Root's example
+#
+# ------------------------------------------------------------------------ #
+
+# -- Data -- #
+# declare variables and constants
+objFile_ToDoList = "ToDoList.txt"   # An object that represents a file
+strData = ""  # A row of text data from the file
+dicRow = {}    # A row of data separated into elements of a dictionary {Task,Priority}
+lstTable = []  # A list that acts as a 'table' of rows
+strMenu = ""   # A menu of user options
+strChoice = "" # A Capture the user option selection
+
+
+def ReadFile():
+
+    # Function to read the file ToDoList.txt into memory for manipulation and display later in the program
+    # Try for seeing if file is in home dictionary of program
+    try:
+        objFile_ToDoList = open("ToDoList.txt", "r")
+        for row in objFile_ToDoList:
+            lstRow = row.split(",")
+            dicRow = {"Task" : lstRow[0], "Priority" : lstRow[1].strip()}
+            lstTable.append(dicRow)
+            print(lstTable)
+        objFile_ToDoList.close()
+
+    # Print error statement prompting user to locate file needed to run program
+    except:
+        print("File not found. Locate ToDoList.txt and ensure file is in the same directory as program.")
+
+def UserMenu():
+
+    # Menu function to display user choices
+    print("""
+    Menu of Options
+    1) Show current data
+    2) Add a new item.
+    3) Remove an existing item.
+    4) Save Data to File
+    5) Exit Program
+    """)
+    strChoice = str(input("Which option would you like to perform? [1 to 5] - "))
+    print()  # adding a new line for looks
+
+
+# -- Processing -- #
+# Step 1 - When the program starts, load the any data you have
+# in a text file called ToDoList.txt into a python list of dictionaries rows (like Lab 5-2)
+# Used a function to read the file in case of future need and calling upon reading the file
+
+ReadFile()
+
+# -- Input/Output -- #
+# Step 2 - Display a menu of choices to the user
+while (True):
+
+    # Calls function UserMenu to display menu of user options
+    UserMenu()
+
+    # Step 3 - Show the current items in the table
+    if strChoice.strip() == '1':
+
+        # Prints the current contents of lstTable after reading in the data from the existing file immediately upon
+        # opening program and/or after user has added information to be added to the file
+        print(lstTable, "List with Dictionary objects")
+        continue
+
+    # Step 4 - Add a new item to the list/Table
+    elif strChoice.strip() == '2':
+
+        # Prompts user to add in the task name (one word only) and the priority level (low, medium, high, urgent) to be
+        # accomplished. Prompts user to type in exit to return to main menu
+
+        # while loop prompting user to return to main menu by typing exit
+        while(True):
+            print("Type exit at any prompt to exit to main menu.")
+            strTask = input("Task? (what needs to be done, one word only) ")
+
+            if strTask.lower() == "exit":
+                break
+
+            strPriority = input("Priority? (low, medium, high, urgent) ")
+
+            if strPriority.lower() == "exit":
+                break
+
+            # Appends to lstTable
+            lstTable.append({"Task" : strTask, "Priority" : strPriority})
+
+        continue
+
+    # Step 5 - Remove a new item from the list/Table
+    elif strChoice.strip() == '3':
+        # Prompts user to enter task to remove or exit to quit.
+        strRemove = input("Enter the task you would like to remove. Type exit to quit.")
+
+        # Loop to allow user to remove multiple items if desired. "Exit" quits to main menu. CHECK THIS.
+        while (True):
+            # If check to determine if user typed "exit"
+            if strRemove.lower() == "exit":
+                break
+
+            # Else performs user desired action of removing item from list, if the item is found.
+            else:
+                # Pulls in data from lstTable to prepare for check
+                for row in lstTable:
+                    # If check to determine if task is found.
+                    if row["Item"].lower == strRemove.lower():
+                        lstTable.remove(row)
+                        print("Task removed.")
+                    # Else statement when item not found. Should go back to main menu. CHECK THIS.
+                    else:
+                        print("Task not found. Please check the data using Option 1 from the Main Menu.")
+                break
+        continue
+
+    # Step 6 - Save tasks to the ToDoToDoList.txt file
+    elif strChoice.strip() == '4':
+        # Opens and writes to ToDoList.txt, then closes the file.
+
+        # Open ToDoList.txt
+        objFile_ToDoList = open("ToDoList.txt", "w")
+        objFile_ToDoList.write("Task, Priority" + '\n')
+
+        # Iterate through lstTable list to write each item to the file. Overwrites existing file data
+        for row in lstTable:
+            objFile_ToDoList.write(str(row["Item"]) + ', ' + str(row["Task"]) + '\n')
+
+        # closes file
+        objFile_ToDoList.close()
+        print("Items saved succesfully!")
+
+        continue
+
+    # Step 7 - Exit program
+    elif strChoice.strip() == '5':
+        # Exits the program. Chose exit rather than break to try since the user is wanting to exit, not just break out
+        # of the while loop
+        # break  # and Exit the program
+        exit()
+
+    # Option if user enters anything other than 1 - 5
+    else:
+        print("Please choose only option 1 - 5.")
